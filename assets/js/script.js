239365587eba59491cdf359badf463f50db18bb5
nathallyet <nathallyet@gmail.com>
2022-10-07 15:25:33 -0300

    map movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,32 @@
-const movies = [ 
-    { year: 2008, title: 'Avatar', publisher: '20th Century Fox', ticketSales: '2,907,796,489' },
-    { year: 1997, title: 'Titanic', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '2,797,501,328' },
-    { year: 2012, title: 'The Avengers: endgame', publisher: 'Marvel Studios', ticketSales: '2,187,535,296' },
-    { year: 2015, title: 'Star Wars: The Force Awakens', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '2,068,223,624' },
-    { year: 2018, title: 'Avengers: Infinity War', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '2,048,359,754' },
-    { year:2021, title: 'Spider-man: No Way Home', publisher:'Sony Pictures Entertainment', ticketSales : '1,916,044,248' },    
-    { year: 2015, title: 'Jurassic World', publisher: 'Universal Pictures', ticketSales: '1,671,537,444' },
-    { year: 2019, title: "The Lion King", publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,656,943,394' },
-    { year: 2018, title: "Marvel's The Avengers", publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,518,812,988' },
-    { year: 2015, title: 'Furious ', publisher: 'Universal Pictures', ticketSales: '1,516,045,911' },
-    { year: 2022, title: 'Top Gun Maverick', publisher: 'Universal Pictures', ticketSales: '1,476,676,720' },
-    { year: 2019, title: 'Frozen II', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,450,026,933' },
-    { year: 2015, title: 'Avengers: Age of Ultron', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,402,809,540' },
-    { year: 2018, title: 'Black Panther', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,347,280,838' }
- ];
+const movies = [
+  { year: 2008, title: 'Avatar', publisher: '20th Century Fox', ticketSales: '2,907,796,489' },
+  { year: 1997, title: 'Titanic', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '2,797,501,328' },
+  { year: 2012, title: 'The Avengers: endgame', publisher: 'Marvel Studios', ticketSales: '2,187,535,296' },
+  { year: 2015, title: 'Star Wars: The Force Awakens', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '2,068,223,624' },
+  { year: 2018, title: 'Avengers: Infinity War', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '2,048,359,754' },
+  { year:2021, title: 'Spider-man: No Way Home', publisher:'Sony Pictures Entertainment', ticketSales : '1,916,044,248' },
+  { year: 2015, title: 'Jurassic World', publisher: 'Universal Pictures', ticketSales: '1,671,537,444' },
+  { year: 2019, title: "The Lion King", publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,656,943,394' },
+  { year: 2018, title: "Marvel's The Avengers", publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,518,812,988' },
+  { year: 2015, title: 'Furious ', publisher: 'Universal Pictures', ticketSales: '1,516,045,911' },
+  { year: 2022, title: 'Top Gun Maverick', publisher: 'Universal Pictures', ticketSales: '1,476,676,720' },
+  { year: 2019, title: 'Frozen II', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,450,026,933' },
+  { year: 2015, title: 'Avengers: Age of Ultron', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,402,809,540' },
+  { year: 2018, title: 'Black Panther', publisher: 'Walt Disney Studios Motion Pictures', ticketSales: '1,347,280,838' }
+];
+
+const moviesDOM = document.querySelector('#mapMovies');
+
+const mapMovies = movies.map((movie, index) => {
+  return (
+    moviesDOM.innerHTML +=
+    `<tr>
+      <td>${index + 1}</td>
+      <td>${movie.year}</td>
+      <td>${movie.title}</td>
+      <td>${movie.publisher}</td>
+      <td>${movie.ticketSales}</td>
+    </tr>`
+  )
+});
+
